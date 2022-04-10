9bfc929eaba23d163e60251e2f207f8d65744956
MeLea Hetrick <meletrick@gmail.com>
2022-04-10 14:41:13 -0500

    20220410

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -1,16 +1,25 @@
 library(dplyr)
+
+#Deliverable 1
 mechacar <- read.csv(file='MechaCar_mpg.csv')
 mechacarDF <- as.data.frame(mechacar)
 summary(mechacarDF)
 mpg_eval <- lm(mpg ~ ., data = mechacarDF)
-AWD_eval <- lm(AWD ~., data = mechacarDF)
-length_eval <- lm(vehicle_length ~ ., data = mechacarDF)
-weight_eval <- lm(vehicle_weight ~ ., data = mechacarDF)
-spoiler_eval <- lm(spoiler_angle ~ ., data = mechacarDF)
-clearance_eval <- lm(ground_clearance ~ ., data = mechacarDF)
 summary(mpg_eval)
-summary(AWD_eval)
-summary(length_eval)
-summary(weight_eval)
-summary(spoiler_eval)
-summary(clearance_eval)
+
+#Deliverable 2
+suspension_coil <- read.csv(file='Suspension_Coil.csv')
+total_summary <- suspension_coil %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI))
+coil_lot <- suspension_coil %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI))
+
+t.test(suspension_coil$PSI, mu=15000)
+
+#Deliverable 3
+coil_lot1 <- subset(suspension_coil, Manufacturing_Lot =='Lot1')
+t.test(coil_lot1$PSI, mu=1500)
+
+coil_lot2 <- subset(suspension_coil, Manufacturing_Lot =='Lot2')
+t.test(coil_lot2$PSI, mu=1500)
+
+coil_lot3 <- subset(suspension_coil, Manufacturing_Lot =='Lot3')
+t.test(coil_lot3$PSI, mu=1500)
